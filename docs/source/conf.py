9ef494c8df997b1ea946bcbfcc0409f114f55964
Ghostling225 <wojcikadam742@gmail.com>
2022-05-04 11:04:21 +0200

    Modify example for CLImod

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CLImod'
+copyright = '2022, Ghostling225'
+author = 'Ghostling225'
 
 release = '0.1'
 version = '0.1.0'
